8d17eb355f7baaa618c67f1bfc2dae337149a430
Nicholas <nicho.tjornelund@gmail.com>
2018-12-12 01:09:07 +0100

    AST creator template js

--- NS-compiler-js/visitor.js
+++ NS-compiler-js/visitor.js
@@ -1,14 +1,101 @@
-var GrammarVisitor = require('GrammarVisitor').GrammarVisitor;
+var GrammarVisitor = require('./GrammarVisitor').GrammarVisitor;
 
-function GrammarVisitor() {
-    antlr4.tree.ParseTreeVisitor.call(this);
-    return this;
+class visitor extends GrammarVisitor {
+
+    visitProgram (ctx) {
+        return {txt: ctx.getText()}
+    }
+
+    
+    visitAssignstatement(ctx) {
+    }
+
+
+
+    visitIfstatement(ctx) {
+    }
+
+
+
+    visitReturnstatement(ctx) {
+    }
+
+
+
+    visitCallstatement(ctx) {
+    }
+
+
+
+    visitAssigneval(ctx) {
+    }
+
+
+
+    visitAssignfunction(ctx) {
+    }
+
+
+
+    visitIff(ctx) {
+    }
+
+
+
+    visitReturnn(ctx) {
+    }
+
+
+
+    visitCall(ctx) {
+    }
+
+
+
+    visitEvalbinop(ctx) {
+    }
+
+
+
+    visitEvalcall(ctx) {
+    }
+
+
+
+    visitBinopvalue(ctx) {
+    }
+
+
+
+    visitBinopbinop(ctx) {
+    }
+
+
+
+    visitSign(ctx) {
+    }
+
+
+
+    visitFunctionn(ctx) {
+    }
+
+
+
+    visitArg(ctx) {
+    }
+
+
+
+    visitBlock(ctx) {
+    }
+
+
+
+    visitValue(ctx) {
+    }
+
+
 }
 
-GrammarVisitor.prototype = Object.create(antlr4.tree.ParseTreeVisitor.prototype);
-GrammarVisitor.prototype.constructor = GrammarVisitor;
-
-GrammarVisitor.prototype.visitProgram = function(ctx) {
-    console.log(ctx.toString())
-    console.log(ctx.statement())
-};
+module.exports = visitor;
