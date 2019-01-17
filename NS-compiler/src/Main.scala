86142e47fc933eb98027006ffadc1dd4c04cb0e5
Nicholas <nicho.tjornelund@gmail.com>
2019-01-17 19:49:35 +0100

    type

--- NS-compiler/src/Main.scala
+++ NS-compiler/src/Main.scala
@@ -4,6 +4,10 @@ object Main {
         val glue = new ANTLRHandler
         val rootnode = glue.getNodeStructure("6")
         val converter = new JavaToScalaBind
-        val tree = converter.convert(rootnode)
+        val semanticChecker = new Semant
+
+
+        var tree = converter.convert(rootnode)
+        tree = semanticChecker.typeAnnotate(tree)
     }
 }
--- NS-compiler/src/Semant.scala
+++ NS-compiler/src/Semant.scala
@@ -1,13 +1,67 @@
+
 class Semant {
 
-    def typeAnnotate (tree:Tree): Unit = {
+    def typeAnnotate(tree: Tree): Tree = {
         tree match {
-            case programNode(children) => {
+            case programNode(children, ns) => {
                 val c = children.map(typeAnnotate(_))
-                return programNode(c)
+                return programNode(c, ns)
             }
-            case assignNode(id,body) => {
 
+            case ifNode(cond, body, els, ns) => {
+                return ifNode(typeAnnotate(cond), typeAnnotate(body), typeAnnotate(els), "")
+            }
+
+            case assignNode(id, body, ns) => {
+                val newbody = typeAnnotate(body)
+                val ty = newbody.nstype
+                return assignNode(id, newbody, ty)
+            }
+
+            //TODO:type
+            case incNode(id, body, sign, ns) => {
+                val value = valueNode(id, false, true, "")
+                val newbody = binopNode(value, sign, typeAnnotate(body), "")
+                return assignNode(id, newbody, "")
+            }
+
+            case binopNode(left, sign, right, ns) => {
+                val newleft = typeAnnotate(left)
+                val newright = typeAnnotate(right)
+                return binopNode(newleft, sign, newright, newleft.nstype)
+            }
+
+            case whileNode(cond, body, ns) => {
+                return whileNode(typeAnnotate(cond), typeAnnotate(body), "")
+            }
+
+            case callNode(id, args, ns) => {
+
+            }
+
+            case blockNode(children, ns) => {
+                val newchildren = children.map(typeAnnotate(_))
+
+                var blockType: String = null
+                newchildren.map((x) => x match {
+                        case returnNode(_,ns) => blockType = ns
+                        case _ => Nil//error
+                    })
+
+                /*val search = (x: Tree) => x match {
+                    case returnNode(_, _) => true
+                    case _ => false
+                }
+                val ret = newchildren.find(search).get
+                val returnNode(_, ns) = ret
+                */
+
+                return blockNode(newchildren, blockType)
+            }
+
+            case functionNode(args, body, ns) => {
+                val newbody = typeAnnotate(body)
+                return functionNode(args, newbody,newbody.nstype)
             }
         }
     }
