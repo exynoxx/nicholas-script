6a108f82098981e4eeb32d0eb5e723c9e00ca9c6
Nicholas <nicho.tjornelund@gmail.com>
2019-07-04 14:58:36 +0200

    seperate file

--- parser2/src/main/scala/main.scala
+++ parser2/src/main/scala/main.scala
@@ -3,39 +3,30 @@ import scala.io.Source
 object mainObj extends App {
 
 	def printArray(x: Array[String]) = {
-		val str = p.transitionList.foldLeft("") { (a: String, b: String) => a + b + " " }
+		val str = x.foldLeft("") { (a: String, b: String) => a + b + " " }
 		println(str)
 	}
 
-	def readFile(filename: String): Array[String] = {
+	def readFile(filename: String): String = {
 		val bufferedSource = Source.fromFile(filename)
-		val allrules = bufferedSource.getLines().mkString
+		val ret = bufferedSource.getLines().mkString
 		bufferedSource.close
-		allrules.split("[$\n]")
+		ret
 	}
 
 
 	val l = new lexer;
-	val p = new parser;
+	val p = new parser(false);
 
-	val rules = readFile("src/main/scala/grammer.g")
+	val rules = readFile("src/main/scala/grammer.g").split("[$\n]")
 	rules.foreach(s => p.addRule(s))
 	/*
-	p.addRule("statement::=if|while|assign SEMICOLON")
-	p.addRule("assign::=def|defshort")
-	p.addRule("def::=VAR STRING(*) EQ binop|VAR STRING(*) EQ func")
-	p.addRule("defshort::=STRING(*) DEFEQ binop|STRING(*) DEFEQ func")
-	p.addRule("binop::=value|value op binop")
-	p.addRule("op ::= PLUS | MINUS | MULT | DIV | AND | OR | GE | LE | GEQ | LEQ | NEQ")
-	p.addRule("value::=INT(*)")
-
 	func ::= LPAREN args RPAREN IMPL fbody
 	args ::= (STRING(*))*...............TODO
 	fbody ::= binop | if | while | block
 	 */
-	p.rules.foreach { case (k, v) => println(k.foldLeft("") { (a: String, b: String) => a + b + " " } + " -> " + v) }
 
-	val input = "b := 5+5;"
+	val input = readFile("src/main/scala/test.ns")
 	val parseTree: Tree = p.parse(l.tokenize(input))
 	print(parseTree)
 
