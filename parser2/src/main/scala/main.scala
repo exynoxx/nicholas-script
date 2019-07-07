588fa932111d4ccda787ae1b1cbf050e210d46b1
Nicholas <nicho.tjornelund@gmail.com>
2019-07-07 15:26:34 +0200

    while

--- parser2/src/main/scala/main.scala
+++ parser2/src/main/scala/main.scala
@@ -1,7 +1,5 @@
 import scala.io.Source
 import scala.util.parsing.combinator._
-import scala.util.parsing.combinator.syntactical.StandardTokenParsers
-import scala.util.parsing.combinator.Parsers
 class TreePrinter {
 	def printMinus(sym: String, depth: Int): String = {
 		val spaces = (0 to depth).map(x => sym).mkString("")
@@ -34,7 +32,10 @@ class TreePrinter {
 				"ifNode\n" +
 				recursion(c, depth + increment) +
 				recursion(b, depth + increment)
-
+			case whileNode(c,b,ns) => printMinus("-", depth) +
+				"whileNode\n" +
+				recursion(c, depth + increment) +
+				recursion(b, depth + increment)
 		}
 	}
 
@@ -63,14 +64,17 @@ class Parser extends RegexParsers {
 
 	def start: Parser[Tree] = rep(statement) ^^ { case s => blockNode(s, "") }
 
-	def defstatement: Parser[Tree] = "var" ~ word ~ "=" ~ exp ~ ";" ^^ { case s1 ~ valueNode(s2,_) ~ s3 ~ t ~ s4 => assignNode(s2, t, "") } | word ~ ":=" ~ exp ~ ";" ^^ { case valueNode(s1,_) ~ s2 ~ t ~ s3 => assignNode(s1, t, "") }
+	def defstatement: Parser[Tree] = "var" ~ word ~ "=" ~ exp ~ ";" ^^
+		{ case s1 ~ valueNode(s2,_) ~ s3 ~ t ~ s4 => assignNode(s2, t, "") } |
+		word ~ ":=" ~ exp ~ ";" ^^ { case valueNode(s1,_) ~ s2 ~ t ~ s3 => assignNode(s1, t, "") }
 
-	def ifstatement: Parser[Tree] =
-		"if" ~ "(" ~ binop ~ ")" ~ (exp | block) ~ opt("else" ~ (exp | block)) ^^ { case _ ~ _ ~ b ~ _ ~ e1 ~ Some(_ ~ e2) => ifNode(b, e1, Some(e2), "")
-		case _ ~ _ ~ b ~ _ ~ e1 ~ None => ifNode(b, e1, None, "")
+	def ifstatement: Parser[Tree] = "if" ~ "(" ~ binop ~ ")" ~ (exp | block) ~ opt("else" ~ (exp | block)) ^^
+		{ 	case _ ~ _ ~ b ~ _ ~ e1 ~ Some(_ ~ e2) => ifNode(b, e1, Some(e2), "")
+			case _ ~ _ ~ b ~ _ ~ e1 ~ None => ifNode(b, e1, None, "")
 		}
+	def whilestatement: Parser[Tree] = "while" ~ "(" ~ binop ~ ")" ~ (exp | block) ^^ {case s1 ~s2~b~s3~e => whileNode(b,e,"")}
 
-	def statement: Parser[Tree] = ifstatement | defstatement ^^ { case s => s }
+	def statement: Parser[Tree] = ifstatement | whilestatement | defstatement ^^ { case s => s }
 }
 
 object main {
