9e26842eea35b3db6394db07774bd8e3cfef9da2
Nicholas <nicho.tjornelund@gmail.com>
2019-08-24 15:33:33 +0200

    void func + reassignment

--- parser/out/out.js
+++ parser/out/out.js
@@ -22,10 +22,18 @@ i = i+1
 return ret
 }
 
+var f2 = function(x,y) {
+print(f1(x,y))
+}
+
 var ran2 = 1+3
 var d = f1(5,ran2)
+var ran3 = 1+3
+f2(5,ran3)
 print(d)
 var short = Array.from(new Array(5-1), (x,i) => i + 1)
 var long = [1,2,3,4]
 print(short)
 print(long)
+long = Array.from(new Array(2-1), (x,i) => i + 1)
+long = short
