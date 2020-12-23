a8a6fd4495c9c2a8070600a79093d786d0b826e6
Nicholas <nicho.tjornelund@gmail.com>
2020-12-23 11:40:10 +0100

    bool testing for now

--- parser/out/output.rs
+++ parser/out/output.rs
@@ -1,17 +1,17 @@
 fn main(){
 fn print(x:String) {
  println!("{}",x); }
-let mut a:i32 = 5;
-if (1+1>2||a<a.pow(2)) {
-}
-if (a<=a%2) {
-let mut b:i32 = a;
+let mut a:Vec<i32> = (1..5).collect();
+let mut b:Vec<i32> = vec![1,2,3,4];
+if (1>5) {
+print(a);
 }
 else {
-let mut b:i32 = (a.pow(2)/2);
+print(b);
 }
-let mut c:i32 = 10;
-while (c>0||c+a%2==3&&c.pow(5)>0||(1+1)*3*(a%2)<=450) {
-c = (c-1);
-}
+let mut c:i32 = 5;
+let mut d:i32 = (1+c+5*30/4+b[0]);
+let mut e:i32 = (1+1*3+2.pow(10)+2/4);
+let mut f:i32 = ((1+1)*11*(1+(5%2)*(1+(3/2)+a[2]*(a[3]+a[2]))));
+let mut g:bool = (4||a>=0&&a[1]<a[2]);
 }
--- parser/src/main/scala/main.scala
+++ parser/src/main/scala/main.scala
@@ -29,7 +29,7 @@ object main {
 		val t = new TypeChecker
 		val cg = new CodeGenRust
 
-		val inputFile = "src/main/scala/loop.ns"
+		val inputFile = "src/main/scala/basic.ns"
 		val outputFile = "out/output.rs"
 
 		//JS
