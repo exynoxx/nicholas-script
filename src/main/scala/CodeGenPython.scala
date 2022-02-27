875c4fd10d2b7f29a7ed43869d76e6d141f362fa
Nicholas <nicholas@XPS>
2022-02-27 11:57:25 +0100

    begin codegen

---
+++ src/main/scala/CodeGenPython.scala
@@ -0,0 +1,13 @@
+class CodeGenPython {
+	def recurse(t: Tree): String = t match {
+		case integerNode(x) => x.toString
+		case boolNode(x) => x.toString
+		case stringNode(x) => x
+		case wordNode(x) => x
+		//TODO: python operator prescedence different from parse
+		case binopNode(op, left, right) => recurse(left) + op + recurse(right)
+		case assignNode(id, b) => recurse(id) + "=" + recurse(b)
+		case arrayNode(elements) => "[" + elements.map(recurse).mkString(",") + "]"
+		case accessNode(array, idx) => recurse(array) + "[" + recurse(idx) + "]"
+	}
+}
