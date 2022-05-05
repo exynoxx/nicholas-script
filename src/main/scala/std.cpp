0f9741296bb008084b9ca63c278dd377b15a96b6
Nicholas <nicholas@XPS>
2022-05-05 20:56:45 +0200

    organize, rename. all _NS

--- src/main/scala/std.cpp
+++ src/main/scala/std.cpp
@@ -4,20 +4,23 @@
 #include <vector>
 #include <cstring>
 
-struct _NS_var;
+
+/*   FUNDAMENTAL ENTITIES   */
+
+struct _NS_var_struct;
 enum _NS_enum {VOID, INT,STRING,BOOL,ARRAY,FUNCTION0,FUNCTION1,FUNCTION2};
 
-typedef _NS_var* var;
-typedef var (*_NSfunc2)(var x, var y);
-typedef var (*_NSfunc1)(var x);
-typedef var (*_NSfunc0)();
+typedef _NS_var_struct* _NS_var;
+typedef _NS_var (*_NSfunc2)(_NS_var x, _NS_var y);
+typedef _NS_var (*_NSfunc1)(_NS_var x);
+typedef _NS_var (*_NSfunc0)();
 
 
 union _NS_value{
     int i;
     std::string *s;
     bool b;
-    std::vector<var> *array;
+    std::vector<_NS_var> *array;
     _NSfunc0 f0;
     _NSfunc1 f1;
     _NSfunc2 f2;
@@ -28,114 +31,119 @@ union _NS_value{
 
 };
 
-struct _NS_var{
+struct _NS_var_struct{
     _NS_enum type;
     _NS_value* value;
 
-    _NS_var(_NS_enum t,_NS_value* v){
+    _NS_var_struct(_NS_enum t,_NS_value* v){
         type=t;
         value=v;
     }
-    ~_NS_var(){
+    ~_NS_var_struct(){
         std::cout<<"destructing:"<<value->i<<std::endl;
         if (type == ARRAY) delete value->array;
         delete value;
     }
 };
 
-var create_var(int i){
-    return new _NS_var(INT,new _NS_value{.i=i});
+_NS_var _NS_create_var(int i){
+    return new _NS_var_struct(INT,new _NS_value{.i=i});
 }
-var create_var(std::vector<var> *a){
+_NS_var _NS_create_var(std::vector<_NS_var> *a){
     auto value = new _NS_value; 
     value->array = a;
-    return new _NS_var(ARRAY,value);
+    return new _NS_var_struct(ARRAY,value);
 }
-var create_var(std::initializer_list<var> init){
+_NS_var _NS_create_var(std::initializer_list<_NS_var> init){
     auto value = new _NS_value; 
-    value->array = new std::vector<var>(init.begin(),init.end());
-    return new _NS_var(ARRAY,value);
+    value->array = new std::vector<_NS_var>(init.begin(),init.end());
+    return new _NS_var_struct(ARRAY,value);
 }
-var create_var(_NSfunc0 f){
+_NS_var _NS_create_var(_NSfunc0 f){
     auto value = new _NS_value; 
     value->f0 = f;
-    return new _NS_var(FUNCTION0,value);
+    return new _NS_var_struct(FUNCTION0,value);
 }
-var create_var(_NSfunc1 f){
+_NS_var _NS_create_var(_NSfunc1 f){
     auto value = new _NS_value; 
     value->f1 = f;
-    return new _NS_var(FUNCTION1,value);
+    return new _NS_var_struct(FUNCTION1,value);
 }
-var create_var(_NSfunc2 f){
+_NS_var _NS_create_var(_NSfunc2 f){
     auto value = new _NS_value; 
     value->f2 = f;
-    return new _NS_var(FUNCTION2,value);
+    return new _NS_var_struct(FUNCTION2,value);
 }
 
 
-auto adders = new _NSfunc2[8*8+8];
+/*   MISC   */
 
-
-
-
-var _NSmap(var f,var array,var x)
+_NS_var _NSmap(_NS_var f,_NS_var array,_NS_var x)
 {
-    std::vector<var> *tmp = new std::vector<var>(array->value->array->size());
+    std::vector<_NS_var> *tmp = new std::vector<_NS_var>(array->value->array->size());
     for (size_t i = 0; i < array->value->array->size(); i++)
     {
         tmp->at(i) = f->value->f2(x,array->value->array->at(i));
     }
     //std::transform(array.value.array->begin(), array.value.array->end(),x, tmp->begin(), f.value.f2);
-    return create_var(tmp);
+    return _NS_create_var(tmp);
 }
 
 
-var _NSadd(var x, var y){
-    auto adder = adders[x->type*8+y->type];
+
+
+
+/*   EVERY OPERATOR   */
+auto _NS_binary_ops = new _NSfunc2[8*8+8];
+
+
+_NS_var _NSadd(_NS_var x, _NS_var y){
+    auto adder = _NS_binary_ops[x->type*8+y->type];
     return adder(x,y);
 }
 
 
-var intlistadder(var x, var y){
+
+
+
+
+/*  BINARY OPERATIONS (EACH COMBINATION)  */
+
+_NS_var _NS_int_list_adder(_NS_var x, _NS_var y){
     _NSfunc2 f = &_NSadd;
-    return _NSmap(create_var(f), y, x);
+    return _NSmap(_NS_create_var(f), y, x);
 }
 
-var stdadder (var x, var y){
-    //delete x;
-    //delete y;
-    return create_var(x->value->i+y->value->i);
+_NS_var _NS_std_adder (_NS_var x, _NS_var y){
+    return _NS_create_var(x->value->i+y->value->i);
 }
+
+
+
+
+
     
 int main(){
-    adders[8*1+1] = &stdadder;
-    adders[8*1+4] = &intlistadder;
+    _NS_binary_ops[8*1+1] = &_NS_std_adder;
+    _NS_binary_ops[8*1+4] = &_NS_int_list_adder;
     
     std::cout<<"x"<<"\n";
 
-    auto one = create_var(1);
-    auto two = create_var(2);
+    auto one = _NS_create_var(1);
+    std::cout << one->value->i << "\n";
+    auto two = _NS_create_var(2);
 
     auto x = _NSadd(one,two);
     std::cout << x->value->i << "\n";
     
-    // std::cout<<"four"<<"\n";
-    // auto four = _NS_var(4);
-
-    std::cout<<"v"<<"\n";
-    auto four = create_var(4);
-    std::vector<var> *v = new std::vector<var>(4);
-    v->at(0) = four;
-    v->at(1) = four;
-    v->at(2) = four;
-    v->at(3) = four;
-
-    std::cout<<"assign v"<<"\n";
-    x = create_var(v);
-    std::cout << x->value->array->at(1)->value->i << "\n";
-    x = create_var({four,four,four});
+    auto four = _NS_create_var(4);
+    x = four;
+    std::cout << x->value->i << "\n";
+    x = _NS_create_var({four,four,four});
     std::cout << x->value->array->at(1)->value->i << "\n";
     x = _NSadd(one,x);
+    std::cout << x->value->array->at(1)->value->i << "\n";
+    x = _NSadd(two,x);
     std::cout << x->value->array->at(1)->value->i << "\n";
 } 
 
