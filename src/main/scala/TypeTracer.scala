4a8507b81f0809c41aa199e33f32e3b285394cdd
Nicholas <nicho.tjornelund@gmail.com>
2022-05-21 14:16:52 +0200

    working tracer on subset

--- src/main/scala/TypeTracer.scala
+++ src/main/scala/TypeTracer.scala
@@ -4,13 +4,13 @@ import scala.collection.mutable.ListBuffer
 case class typedNode(node: Tree, typ: Type) extends Tree
 
 class TypeTracer {
-	val functionCallArgs = mutable.HashMap[String,ListBuffer[List[Type]]]()
+	val functionCallArgs = mutable.HashMap[String, ListBuffer[List[Type]]]()
 	var graph = mutable.HashMap[String, Type]()
 
-	def lookupType(left: Type, right: Type): Type = (left,right) match {
-		case (intType(),intType()) => intType()
-		case (boolType(),boolType()) => boolType()
-		case (stringType(),stringType()) => stringType()
+	def lookupType(left: Type, right: Type): Type = (left, right) match {
+		case (intType(), intType()) => intType()
+		case (boolType(), boolType()) => boolType()
+		case (stringType(), stringType()) => stringType()
 		case (arrayType(a), arrayType(b)) => arrayType(a)
 		case _ => intType()
 	}
@@ -26,7 +26,7 @@ class TypeTracer {
 			val ll = dfs1(l)
 			val rr = dfs1(r)
 			typedNode(binopNode(op, ll, rr), lookupType(ll.typ, rr.typ))
-		case functionNode(id,_,_) =>
+		case functionNode(id, _, _) =>
 			typedNode(node, unknownType())
 		case assignNode(wordNode(id), body) =>
 			val recurs = dfs1(body)
@@ -39,7 +39,7 @@ class TypeTracer {
 			typedNode(assignNode(wordNode(id), recurs), recurs.typ)
 		case callNode(wordNode(id), args) =>
 			val argTypes = args.map(dfs1)
-			functionCallArgs.getOrElseUpdate(id,new ListBuffer()).addOne(argTypes.map(_.typ))
+			functionCallArgs.getOrElseUpdate(id, new ListBuffer()).addOne(argTypes.map(_.typ))
 			typedNode(callNode(wordNode(id), argTypes), unknownType())
 		case ifNode(c, b, None) =>
 			val body = dfs1(b)
@@ -50,64 +50,88 @@ class TypeTracer {
 		case returnNode(exp) =>
 			val body = dfs1(exp)
 			typedNode(returnNode(body), body.typ)
+		case typedNode(exp, ty) =>
+			typedNode(exp, ty)
 		case x => typedNode(x, voidType())
 	}
 
-	def recurseTypedTree(node: Tree): Tree = node match {
-		case wordNode(x) =>
-			typedNode(node, graph.getOrElse(x, voidType()))
-		case arrayNode(elements) => arrayNode(elements.map(recurseTypedTree))
-		case binopNode(op, l, r) =>
-			binopNode(op,recurseTypedTree(l),recurseTypedTree(r))
-		//paste back in
-		case functionNode(name, args, body) =>
+	def findTypeOfReturn(node: Tree): Type = node match {
+		case blockNode(children) =>
+			val c = children.map(findTypeOfReturn)
+			c.filterNot(_ == voidType()).head
+		case ifNode(_, b, _) => findTypeOfReturn(b)
+		case assignNode(_, body) => findTypeOfReturn(body)
+		case typedNode(returnNode(_), typ) => typ
+		case x => voidType()
+	}
+
+	def recurseTypedTree(node: Tree): typedNode = node match {
+
+		case typedNode(functionNode(name, args, body), _) =>
+			//paste back in
 			val elementOption = functionCallArgs.get(name)
 			elementOption match {
-				case Some(listOfListOfArgs:ListBuffer[List[Type]]) =>
+				case Some(listOfListOfArgs: ListBuffer[List[Type]]) =>
 					//TODO spawn copies
 					val argTypes = listOfListOfArgs(0)
-					val namedArgs = args.zip(argTypes).map{case (node,typ)=>typedNode(node,typ)}
+					val namedArgs = args.zip(argTypes).map { case (node, typ) => typedNode(node, typ) }
 					//TODO send recursively
-					val graphCopy = graph.clone()
-					namedArgs.foreach{case typedNode(wordNode(id),ty) => graph.addOne(id->ty)}
-					val ret = functionNode(name,namedArgs,doScope(body.asInstanceOf[blockNode]))
-					graph = graphCopy
-					ret
-				case None => node
-			}
-		case assignNode(wordNode(id), body) =>
-			assignNode(wordNode(id), recurseTypedTree(body))
-		case reassignNode(wordNode(id), body) =>
-			assignNode(wordNode(id), recurseTypedTree(body))
-		case ifNode(c, b, None) =>
-			ifNode(recurseTypedTree(c),recurseTypedTree(b),None)
-		case ifNode(c, b, Some(e)) =>
-			ifNode(recurseTypedTree(c),recurseTypedTree(b),Some(recurseTypedTree(e)))
-		case returnNode(exp) =>
-			returnNode(recurseTypedTree(exp))
 
+					val graphCopy = graph.clone()
+					namedArgs.foreach { case typedNode(wordNode(id), ty) => graph.addOne(id -> ty) }
+					val fbody = doScope(body.asInstanceOf[blockNode])
+					val fbodyType = findTypeOfReturn(fbody)
+					graph = graphCopy
+					graph.addOne(name -> fbodyType)
+					typedNode(functionNode(name, namedArgs, fbody), fbodyType)
+				case None => typedNode(nullLeaf(), voidType())
+			}
+		case typedNode(assignNode(wordNode(id), body), _) =>
+			val b = recurseTypedTree(body)
+			typedNode(assignNode(wordNode(id), b), b.typ)
+		case typedNode(assignNode(wordNode(id), body), _) =>
+			val b = recurseTypedTree(body)
+			typedNode(assignNode(wordNode(id), b), b.typ)
 
 		case typedNode(callNode(wordNode(id), args), ty) =>
-			if (ty != unknownType()) return node
-			typedNode(callNode(wordNode(id), args.map(recurseTypedTree)), unknownType())
+			if (ty != unknownType()) return typedNode(callNode(wordNode(id), args), ty)
+			typedNode(callNode(wordNode(id), args.map(recurseTypedTree)), graph.getOrElse(id, unknownType()))
 
-		case x => typedNode(x, voidType())
+
+		case typedNode(exp, typ) =>
+			val content = exp match {
+				case wordNode(x) =>
+					typedNode(node, graph.getOrElse(x, voidType()))
+				case arrayNode(elements) => arrayNode(elements.map(recurseTypedTree))
+				case binopNode(op, l, r) =>
+					binopNode(op, recurseTypedTree(l), recurseTypedTree(r))
+				case ifNode(c, b, None) =>
+					ifNode(recurseTypedTree(c), recurseTypedTree(b), None)
+				case ifNode(c, b, Some(e)) =>
+					ifNode(recurseTypedTree(c), recurseTypedTree(b), Some(recurseTypedTree(e)))
+				case returnNode(exp) =>
+					returnNode(recurseTypedTree(exp))
+				case x => x
+			}
+			typedNode(content, typ)
 	}
-
 
 	//TODO: more than 2 runs
 	def doScope(scope: blockNode): Tree = {
-		val firstRun = blockNode(scope.children.map(dfs1))
-		blockNode(firstRun.children.map(dfs1))
+		val firstRun = scope.children.map(dfs1)
+		blockNode(firstRun.map(recurseTypedTree))
 	}
 
-	def process(main: functionNode):Tree= {
-		var elems = main.body.asInstanceOf[blockNode].children
-		val typedBlock = blockNode(elems.map(dfs1))
-		val typedBlock2 =  blockNode(elems.map(recurseTypedTree))
-		functionNode(main.name,main.args,typedBlock2)
+	def process(main: functionNode): Tree = {
+		val body = main.body.asInstanceOf[blockNode]
+		val typedBlock = doScope(body)
+		functionNode(main.name, main.args, typedBlock)
 	}
 }
+
+
+
+
 
 /*
 * x= 5;
