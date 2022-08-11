1f6b2715ece2e08b1f106a479e087b6394a34a98
Nicholas <nicho.tjornelund@gmail.com>
2022-08-11 21:51:08 +0200

    c++ compile, no warnings

--- src/main/scala/std.cpp
+++ src/main/scala/std.cpp
@@ -22,14 +22,14 @@ std::shared_ptr<std::vector<T>> _NS_map_filter(std::shared_ptr<std::vector<T>> l
     //map
     auto mappedResult = new std::vector(filteredResult); //clone
     std::transform(mappedResult->begin(), mappedResult->end(), mappedResult->begin(), fmap);
-    return std::shared_ptr(mappedResult)
+    return std::shared_ptr<std::vector<T>>(mappedResult);
 }
 
 template <typename T>
 std::shared_ptr<std::vector<T>> _NS_concat(std::shared_ptr<std::vector<T>> a, std::shared_ptr<std::vector<T>> b)
 {
 	auto result = new std::vector<T>(a->size()+b->size());
-    result.insert(result->begin(), a->begin(), a->end());
-    result.insert(result->end(), b->begin(), b->end());
-    return std::shared_ptr(result);
+    result->insert(result->begin(), a->begin(), a->end());
+    result->insert(result->end(), b->begin(), b->end());
+    return std::shared_ptr<std::vector<T>>(result);
 }
