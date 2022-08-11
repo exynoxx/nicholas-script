cd380790ffbf63c30f10174bdb64970ee5b2adff
Nicholas <nicho.tjornelund@gmail.com>
2022-08-11 20:59:03 +0200

    implement concat

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -42,6 +42,7 @@ object main {
 		//val in = "qsort = { if list?<=1 | list | (qsort [x|x:list|x<list$0]) ++ [list$0] ++ (qsort [x|x:list|x>list$0]) }; qsort [2,6,20,11,45,6,33,1,7,8];"
 
 		//val in = "x=[1,2,3]; f2={x+y}; y=f2 4"
+
 		val in = "qsort = { if list?<=1 | list | (qsort [x|x:list|x<list$0]) ++ [list$0] ++ (qsort [x|x:list|x>list$0]) }; qsort [2,6,20,11,45,6,33,1,7,8];"
 
 		//TODO: have local variables overlapping with outside variables in list comprehension
--- src/main/scala/std.cpp
+++ src/main/scala/std.cpp
@@ -6,25 +6,30 @@
 #include <memory>
 
 template <typename T>
-int _NS_len(std::shared_ptr<std::vector<T>> list){
+int _NS_len(std::shared_ptr<std::vector<T>> list)
+{
     return list->size();
 }
 
 
 template <typename T, typename G>
-std::shared_ptr<std::vector<T>> _NS_map_filter(std::shared_ptr<std::vector<T>> list,std::function<G(T)> fmap, std::function<bool(T)> ffilter){
-    
+std::shared_ptr<std::vector<T>> _NS_map_filter(std::shared_ptr<std::vector<T>> list,std::function<G(T)> fmap, std::function<bool(T)> ffilter)
+{
     //filter
     std::vector<T> filteredResult;
     std::copy_if(list->begin(), list->end(), std::back_inserter(filteredResult), ffilter);
 
     //map
     auto mappedResult = new std::vector(filteredResult); //clone
-    //auto unary_op = [](int num) {return std::pow(num, 2);};
     std::transform(mappedResult->begin(), mappedResult->end(), mappedResult->begin(), fmap);
+    return std::shared_ptr(mappedResult)
 }
 
 template <typename T>
-std::shared_ptr<std::vector<T>> _NS_concat(std::shared_ptr<std::vector<T>> a, std::shared_ptr<std::vector<T>> b){
-	return a;
+std::shared_ptr<std::vector<T>> _NS_concat(std::shared_ptr<std::vector<T>> a, std::shared_ptr<std::vector<T>> b)
+{
+	auto result = new std::vector<T>(a->size()+b->size());
+    result.insert(result->begin(), a->begin(), a->end());
+    result.insert(result->end(), b->begin(), b->end());
+    return std::shared_ptr(result);
 }
