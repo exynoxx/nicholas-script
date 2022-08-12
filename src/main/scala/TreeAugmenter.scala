exynoxx/nicholas-script

commit 192488fa264fa9a1db315cbdbbe183ba055d617c

dont extract stuff from if, make blockNode in each branch instead

src/main/scala/TreeAugmenter.scala
@@ -233,14 +233,11 @@ class TreeAugmenter extends Stage {
 						val (blockNode(elems), localSym) = recurse(blockNode(elem), symbol)
 						sym ++= localSym
 						blockNode(elems.init :+ reassignNode(wordNode(id), elems.last))
-					case exp =>
-						val b = recurse(exp, symbol)._1 match {
-							case sequenceNode(l) =>
-								preIf ++= l.init
-								l.last
-							case x => x
-						}
-						blockNode(List(reassignNode(wordNode(id), b)))
+					case exp => recurse(exp, symbol)._1 match {
+						case sequenceNode(l) => blockNode(l)
+						case x => blockNode(List(reassignNode(wordNode(id), x)))
+					}
+
 
 				}
 				val nels = elseBody match {
@@ -251,19 +248,13 @@ class TreeAugmenter extends Stage {
 							case blockNode(elem) =>
 								val (blockNode(elems), _) = recurse(blockNode(elem), symbol)
 								Some(blockNode(elems.init :+ reassignNode(wordNode(id), elems.last)))
-							case exp =>
-								val b = recurse(exp, symbol)._1 match {
-									case sequenceNode(l) =>
-										preIf ++= l.init
-										l.last
-									case x => x
-								}
-								Some(blockNode(List(reassignNode(wordNode(id), b))))
-
+							case exp => recurse(exp, symbol)._1 match {
+								case sequenceNode(l) => Some(blockNode(l))
+								case x => Some(blockNode(List(reassignNode(wordNode(id), x))))
+							}
 						}
 				}
 				val result = wordNode(id)
-				//preIf += assignNode(result, integerNode(0)) this is done in codeGen
 				val sequence = preIf :+ ifNode(newCond, newbody, nels, id) :+ result
 				(sequenceNode(sequence.toList), sym)
 

src/main/scala/TypeAugmenter.scala
@@ -104,8 +104,9 @@ class TypeAugmenter extends Stage {
 		case reassignNode(id, body) =>
 			recurse(body) match {
 				case typedNode(sequenceNode(l),ty) => sequenceNode(l.init :+ typedNode(reassignNode(id,l.last),ty))
-				case x => assignNode(id, x)
+				case x => reassignNode(id, x)
 			}
+
 		case blockNode(children) =>
 			val flatChildren = children.map(recurse).flatMap {
 				case typedNode(sequenceNode(l),_) => l

src/main/scala/main.scala
@@ -45,6 +45,7 @@ object main {
 
 		val in = "qsort = { if list?<=1 | list | (qsort [x|x:list|x<list$0]) ++ [list$0] ++ (qsort [x|x:list|x>list$0]) }; qsort [2,6,20,11,45,6,33,1,7,8];"
 
+
 		//TODO: have local variables overlapping with outside variables in list comprehension
 		//TODO: make array inside list comprehension, support
 		//val in = "x=[1,2,3]; x = [\"a\", \"b\"]; a = x+1; y = [\"1\", \"2\"]; I={x}; intArray = y I; b = 1; [b*2+z|z:x]; [1,2,3] {k*b};"

commit 41bd0f462b126ba98fa8549ea5410f446fecfd24

c++ runs: bug introduced in 192488fa264fa9a1db315cbdbbe183ba055d617c

src/main/scala/TreeAugmenter.scala
@@ -234,7 +234,7 @@ class TreeAugmenter extends Stage {
 						sym ++= localSym
 						blockNode(elems.init :+ reassignNode(wordNode(id), elems.last))
 					case exp => recurse(exp, symbol)._1 match {
-						case sequenceNode(l) => blockNode(l)
+						case sequenceNode(l) => blockNode(l.init :+ reassignNode(wordNode(id), l.last))
 						case x => blockNode(List(reassignNode(wordNode(id), x)))
 					}
 
@@ -249,7 +249,7 @@ class TreeAugmenter extends Stage {
 								val (blockNode(elems), _) = recurse(blockNode(elem), symbol)
 								Some(blockNode(elems.init :+ reassignNode(wordNode(id), elems.last)))
 							case exp => recurse(exp, symbol)._1 match {
-								case sequenceNode(l) => Some(blockNode(l))
+								case sequenceNode(l) => Some(blockNode(l.init :+ reassignNode(wordNode(id), l.last)))
 								case x => Some(blockNode(List(reassignNode(wordNode(id), x))))
 							}
 						}