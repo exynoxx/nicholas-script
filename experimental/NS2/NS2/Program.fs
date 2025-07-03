0c0f86ad44c4fd765e6ab61a80f844810ab365b9
Nicholas <nicho.tjornelund@gmail.com>
2025-07-03 20:04:24 +0200

    express compound assign differently

--- experimental/NS2/NS2/Program.fs
+++ experimental/NS2/NS2/Program.fs
@@ -10,7 +10,7 @@ let parse (input:string) =
 let main argv =
     //run_tests()
     
-    let input = "a=0; a += 1";
+    let input = "a=0; b += 1; c ++=1; d=a@1";
     try
         let raw = parse input
         printfn $"Result: %A{raw}"
